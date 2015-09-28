fb72d936d25c452cb15aaeb12f107a721f35700a
Li Yu <li@TEN.local>
2015-09-27 20:03:47 -0500

    added codes for kernel pca

---
+++ kpca/foo.c
@@ -0,0 +1,30 @@
+/*
+ * File: foo.c
+ *
+ * MATLAB Coder version            : 2.6
+ * C/C++ source code generated on  : 10-Oct-2014 21:23:02
+ */
+
+/* Include files */
+#include "rt_nonfinite.h"
+#include "foo.h"
+
+/* Function Definitions */
+
+/*
+ * Thisfunction muliplies a and b
+ * Arguments    : short a
+ *                short b
+ * Return Type  : void
+ */
+void foo(short a, short b)
+{
+  (void)a;
+  (void)b;
+}
+
+/*
+ * File trailer for foo.c
+ *
+ * [EOF]
+ */
